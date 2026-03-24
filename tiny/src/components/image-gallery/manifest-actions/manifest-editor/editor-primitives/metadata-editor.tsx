b9fd5a968cd6a5ba91147447100face394c933e4
Rainer Simon <rainer@rainersimon.io>
2026-03-24 12:24:07 +0100

    Cleanup

--- tiny/src/components/image-gallery/manifest-actions/manifest-editor/editor-primitives/metadata-editor.tsx
+++ tiny/src/components/image-gallery/manifest-actions/manifest-editor/editor-primitives/metadata-editor.tsx
@@ -1,4 +1,4 @@
-import { CirclePlus, Plus, Trash2 } from 'lucide-react';
+import { CirclePlus, Trash2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
